b67cb9731033fb73e820738a7d6a219bfa235065
Keks <keks@htmlacademy.ru>
2022-07-02 15:44:22 +0300

    :heavy_check_mark: Сборка #4

---
+++ 4/js/main.js
@@ -0,0 +1,9 @@
+const randomNumber = function (min, max){
+  if (max<=min) {
+    return 'Сначала введите меньшее число, а потом - большее!';
+  } else {
+    return Math.floor(Math.random()*(max-min));
+  }
+};
+
+randomNumber(0, 1000);
