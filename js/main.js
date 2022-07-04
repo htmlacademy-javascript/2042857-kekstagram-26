34a6491988601b5aba9eb3511167e16f605c0b5e
ERS86 <99912866+ERS86@users.noreply.github.com>
2022-07-05 00:07:07 +0300

    Update main.js
    Прежний код не сохранился. Где-то накосячил...

--- js/main.js
+++ js/main.js
@@ -1,9 +1,1 @@
-const randomNumber = function (min, max){
-  if (max<=min) {
-    return 'Сначала введите меньшее число, а потом - большее!';
-  } else {
-    return Math.floor(Math.random()*(max-min));
-  }
-};
 
-randomNumber(0, 1000);
