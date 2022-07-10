e206df8633a0c5ed652f07cb641b516826cd3536
Рамиль <eseneev.r.s@mail.ru>
2022-07-10 13:07:49 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
-const PHOTO_URLS = [
-    'photos/1.jpg','photos/2.jpg','photos/3.jpg','photos/4.jpg','photos/5.jpg','photos/6.jpg','photos/7.jpg','photos/8.jpg','photos/9.jpg','photos/10.jpg','photos/11.jpg','photos/12.jpg','photos/13.jpg','photos/14.jpg','photos/15.jpg','photos/16.jpg','photos/17.jpg','photos/18.jpg','photos/19.jpg','photos/20.jpg','photos/21.jpg','photos/22.jpg','photos/23.jpg','photos/24.jpg','photos/25.jpg'
-];
-const PHOTO_DESCRIPTIONS = [
-    'Описание 1','Описание 2','Описание 3','Описание 4','Описание 5','Описание 6','Описание 7','Описание 8','Описание 9','Описание 10','Описание 11','Описание 12','Описание 13','Описание 14','Описание 15','Описание 16','Описание 17','Описание 18','Описание 19','Описание 20','Описание 21','Описание 22','Описание 23','Описание 24','Описание 25'
-];
+const getRandomPositiveInteger = (a, b) => {
+    const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+    const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+    const result = Math.random() * (upper - lower + 1) + lower;
+    return Math.floor(result);
+};
+const getRandomArrayElement = (elements) => {
+    return elements[getRandomPositiveInteger(0, elements.length - 1)];
+};
 const AVATAR_IMG = [
     'img/avatar-1','img/avatar-2','img/avatar-3','img/avatar-4','img/avatar-5','img/avatar-6'
 ];
@@ -19,39 +22,66 @@ const COMMENT_MESSAGES = [
     'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ];
 
-const getRandomPositiveInteger = (a, b) => {
-    const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
-    const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
-    const result = Math.random() * (upper - lower + 1) + lower;
-    return Math.floor(result);
+let photo_urls = [];
+let photo_descriptions = [];
+let ids_for_comments = [];
+let commentsArray = [];
+const MAX_COMMENTS_COUNT = 10;
+const MAX_DESCRIPTION_COUNT = 25;
+
+for (let i=1; i<=25; i++){
+    photo_urls.push('photos/'+[i]+'.jpg');
 };
-const getRandomArrayElement = (elements) => {
-    return elements[getRandomPositiveInteger(0, elements.length - 1)];
+let photoUrl = function () {
+    let a;
+    for (let i=1; i<=25; i++) {
+        return a = photo_urls.shift();
+    }
+};
+for (let i=1; i<=25; i++){
+    photo_descriptions.push('Описание'+[i]);
+};
+let photoDescription = function () {
+    let a;
+    for (let i=1; i<=25; i++) {
+        return a = photo_descriptions.shift();
+    }
+};
+for (let i=1; i<=1000; i++){
+    ids_for_comments.push([i]);
+};
+let idComment = function () {
+    let a;
+    for (let i=1; i<1000; i++) {
+        return a = ids_for_comments.shift();
+    }
 };
 
-const COMMENTS = (id) => ({
-    id,
+let newComment = () => ({
+    id: (idComment()),
     avatar: getRandomArrayElement(AVATAR_IMG),
     message: getRandomArrayElement(COMMENT_MESSAGES),
     name: getRandomArrayElement(COMMENTER_NAMES)
 });
+for (let i=1; i<=MAX_COMMENTS_COUNT; i++){
+    commentsArray.push(newComment());
+};
 
-const newPhotoDescription = (id) => ({
+let newPhoto = (id) => ({
     id,
-    url:getRandomArrayElement(PHOTO_URLS),
+    url:(photoUrl()),
     likes: getRandomPositiveInteger(15, 200),
-    description: getRandomArrayElement(PHOTO_DESCRIPTIONS),
-    comment: (COMMENTS())
+    description: (photoDescription()),
+    comment: commentsArray
 });
 
-const createDescriptions = (count) => {
+let createDescriptions = (count) => {
     const photoID = [];
     for (let i=1; i<=count; i++){
-        photoID.push(newPhotoDescription(i));
+        photoID.push(newPhoto(i));
     }
     return photoID;
 };
 
-const MAX_DESCRIPTION_COUNT = 25;
-const descriptions = createDescriptions(MAX_DESCRIPTION_COUNT);
-console.log(descriptions);
+const description = createDescriptions(MAX_DESCRIPTION_COUNT);
+console.log(description);
