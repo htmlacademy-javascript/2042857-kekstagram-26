727767c5b63f3813bb143b766ae2b05be13ad48a
Keks <keks@htmlacademy.ru>
2022-07-14 16:41:14 +0300

    :heavy_check_mark: Сборка #13

---
+++ 13/js/main.js
@@ -0,0 +1,6 @@
+import {createPhoto, MAX_DESCRIPTION_COUNT} from './data.js';
+import {drawMiniatures} from './miniautures.js';
+
+const photos = createPhoto(MAX_DESCRIPTION_COUNT);
+
+drawMiniatures(photos);
---
+++ 13/js/util.js
@@ -0,0 +1,9 @@
+export const getRandomPositiveInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+export const getRandomArrayElement = function(elements) {
+  return elements[getRandomPositiveInteger(0, elements.length - 1)];
+};
